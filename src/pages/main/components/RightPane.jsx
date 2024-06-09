6b6da31ac68d6ace0def48b1b98005e7c309af41
chriswolf <chriswolfdesign@gmail.com>
2024-06-09 10:55:37 -0400

    Adds Project Pane to right slider of webpage

--- src/pages/main/components/RightPane.jsx
+++ src/pages/main/components/RightPane.jsx
@@ -1,23 +1,26 @@
-import {Box} from "@mui/material";
-import {TEXT_GRAY} from "../../../constants/colors";
+import { Box } from "@mui/material";
+import { TEXT_GRAY } from "../../../constants/colors";
 import AboutPane from "./AboutPane";
 import ExperiencePane from "./ExperiencePane";
+import ProjectPane from "./ProjectPane";
 
 const boxStyles = {
   color: TEXT_GRAY,
   marginRight: 8,
   height: 600,
   overflowY: "auto",
-  scrollbarWidth: "none"
-}
+  scrollbarWidth: "none",
+};
 
 function RightPane() {
   return (
     <Box sx={boxStyles}>
       <AboutPane />
       <ExperiencePane />
+      <ProjectPane />
     </Box>
-  )
+  );
 }
 
 export default RightPane;
+
