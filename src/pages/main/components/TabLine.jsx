3892ae18f2c543147cc8853e3b92e568e3235349
chriswolf <chriswolfdesign@gmail.com>
2024-06-09 16:13:57 -0400

    Fixes spacing in tabline

--- src/pages/main/components/TabLine.jsx
+++ src/pages/main/components/TabLine.jsx
@@ -8,13 +8,13 @@ const gridStyle = {
 function TabLine() {
   return (
     <Grid sx={gridStyle} container spacing={1}>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <Tab text="ABOUT" link="/#about" />
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <Tab text="EXPERIENCE" link="/#experience" />
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <Tab text="PROJECTS" link="/#projects" />
       </Grid>
     </Grid>
